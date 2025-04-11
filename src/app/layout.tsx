51b74ec2e3143bce20b0b0800a651ce20da1b891
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-04-11 10:44:48 +0700

    fix: lint issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <Script>
+        <Script id="gtm-script">
           {`
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
