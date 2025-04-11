a5fb36caffe77097409f633e7dfd02cb6c06bd5a
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-04-11 10:39:32 +0700

    chore: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/lib/LanguageContext";
 import ClientLayout from "@/components/ClientLayout";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -74,8 +75,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Script>
+          {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WK925XWL');
+    `}
+        </Script>
+
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK925XWL"
+            height={0} width={0} style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+
         <LanguageProvider>
           <ClientLayout>
             {children}
